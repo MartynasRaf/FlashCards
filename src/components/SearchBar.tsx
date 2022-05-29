7a5e7b55bb36858f55248a5879aa8c24b77b956c
Martynas R <m.rafanavicius@gmail.com>
2022-05-30 01:06:06 +0300

    Update to Search Bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -10,7 +10,14 @@ import {
 const SearchBar = ({value, setValue, style}) => {
   return (
     <View style={[styles.container, style]}>
-      <TextInput value={value} onChangeText={text => setValue(text)} />
+      <TextInput
+        value={value}
+        onChangeText={text => setValue(text)}
+        style={{flex: 1}}
+      />
+      <TouchableOpacity style={styles.searchButton}>
+        <Text style={styles.searchText}>Find</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -23,7 +30,12 @@ const styles = StyleSheet.create({
     borderRadius: 40,
     alignSelf: 'center',
     paddingHorizontal: 20,
+    flexDirection: 'row',
   },
+  searchButton: {
+    justifyContent: 'center',
+  },
+  searchText: {color: 'white', fontWeight: 'bold', fontSize: 25},
 });
 
 export default SearchBar;
